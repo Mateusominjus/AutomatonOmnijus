53ef27a12234ab7ba8af5de6eeed9800da1a1149
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-06-23 18:18:40 -0300

    attt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='automatonOmnijus',
+    packages=['linode_example'],
+    description='wraper de conexão com a Central Omnijus',
+    version='0.1',
+    url='https://github.com/Mateusominjus/AutomatonOmnijus.git',
+    author='Mateus',
+    author_email='mateus.queiroz@omnijus.com.br',
+    keywords=['omnius','lawTec'],
+    install_requires=['requests'],
+)
