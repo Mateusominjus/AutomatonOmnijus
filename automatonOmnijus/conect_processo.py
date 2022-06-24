930389b1e15139befc3885bd3b8ed3373b97039e
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-06-24 15:37:13 -0300

    attt

--- automatonOmnijus/conect_processo.py
+++ automatonOmnijus/conect_processo.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from automatonOmnijus.requisicao import faz_requisicao
 from typing import List
 from automatonOmnijus.rotas import *
@@ -15,3 +16,19 @@ class ConectProcesso:
     def dados_do_processo(self)->dict:
         return faz_requisicao(headers=self._headers,rota=DADOS_DO_PROCESSO)
         
+    def completar_acao(self,nome_da_acao:str,informacoes_do_processo:dict=None,estado:dict=None):
+        body = {}
+        if informacoes_do_processo:
+            body['informacoes_do_processo'] = informacoes_do_processo
+        if estado:
+            body['estado'] = estado
+        
+        novos_headers = deepcopy(self._headers)   
+        novos_headers['acao'] = nome_da_acao
+
+
+        return faz_requisicao(headers=novos_headers,rota=COMPLETAR_ACAO_PENDENTE,body=body)
+     
+     
+    def registrar_tentativa_de_acao(self,nome_da_acao:str):
+        pass 
--- teste.py
+++ teste.py
@@ -4,5 +4,6 @@ from automatonOmnijus import ConectCentral
 
 central = ConectCentral(senha='omnijus01')
 empresa = central.empresa('miojo')
-processo = empresa.processo(12)
-print(empresa.acoes_pendentes())
+processo = empresa.processo(1)
+r = processo.dados_do_processo()
+print(r)
