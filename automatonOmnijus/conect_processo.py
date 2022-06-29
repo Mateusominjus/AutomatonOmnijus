66a92e4c866aa77efd67fe04886a4be9c52cbb86
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-06-28 23:36:25 -0300

    criando sistema  de log

--- automatonOmnijus/conect_processo.py
+++ automatonOmnijus/conect_processo.py
@@ -16,23 +16,26 @@ class ConectProcesso:
     def dados_do_processo(self)->dict:
         return faz_requisicao(headers=self._headers,rota=DADOS_DO_PROCESSO)
         
-    def completar_acao(self,nome_da_acao:str,informacoes_do_processo:dict=None,estado:dict=None):
+    def completar_acao(self,nome_da_acao:str,informacoes_do_processo:dict=None,estado:dict=None,log:dict=None):
         body = {}
 
         if informacoes_do_processo:
             body['processo'] = informacoes_do_processo
         if estado:
             body['estado'] = estado
-        print(body)
-
+        if log:
+            body['log'] = log
+    
         novos_headers = deepcopy(self._headers)   
         novos_headers['acao'] = nome_da_acao
-
 
         return faz_requisicao(headers=novos_headers,rota=COMPLETAR_ACAO_PENDENTE,body=body)
      
      
-    def registrar_tentativa_de_acao(self,nome_da_acao:str):       
+    def registrar_tentativa_de_acao(self,nome_da_acao:str,log:dict=None):       
+        body = {}
         novos_headers = deepcopy(self._headers)   
         novos_headers['acao'] = nome_da_acao
-        return faz_requisicao(headers=novos_headers,rota=REGISTRAR_TENTATIVA_DE_ACAO_PENDENTE)
+        if log:
+            body['log'] = log
+        return faz_requisicao(headers=novos_headers,rota=REGISTRAR_TENTATIVA_DE_ACAO_PENDENTE,body=body)
