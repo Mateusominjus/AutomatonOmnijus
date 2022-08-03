80d50106068c1031eb5d77972ffafb0e9b44479e
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-08-03 20:02:50 -0300

    modificando lógica arquitetural

--- teste.py
+++ teste.py
@@ -4,6 +4,7 @@ from automatonOmnijus import ConexaoCentral
 
 central = ConexaoCentral(senha='omnijus01dev')
 exemplo = central.ambiente('exemplo')
-p1 = exemplo.processo('1',carregar=True)
-p1.novo_documento('teste21.png')
-
+p1 = exemplo.processo('1').criar_processo()
+p1
+r = p1.excluir_processo()
+print(r)
