e1856eccb0e726b1f0f53e032e5d8003498acc25
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-08-03 11:43:16 -0300

    att

--- teste.py
+++ teste.py
@@ -4,7 +4,6 @@ from automatonOmnijus import ConexaoCentral
 
 central = ConexaoCentral(senha='omnijus01dev')
 exemplo = central.ambiente('exemplo')
-p1 = exemplo.processo(1,carregar=True)
-doc = p1.novo_documento('teste15.png')
-print('nome',doc.nome)
-print('hash',doc.hash)
+p1 = exemplo.processo('1',carregar=True)
+p1.novo_documento('teste21.png')
+
